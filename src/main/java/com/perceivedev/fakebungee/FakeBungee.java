6ef2981ae2db3e1d1e5de21ce494597b11bceb26
Rayzr522 <rayzr522@gmail.com>
2016-11-22 08:06:02 -0500

    Project 'FakeBungee' generated
    Generated using PerceivePluginArchetype and custom CLI project generator tool by Rayzr

---
+++ src/main/java/com/perceivedev/fakebungee/FakeBungee.java
@@ -0,0 +1,12 @@
+package com.perceivedev.fakebungee;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class FakeBungee extends JavaPlugin {
+    
+    @Override
+    public void onEnable() {
+        
+    }
+    
+}
